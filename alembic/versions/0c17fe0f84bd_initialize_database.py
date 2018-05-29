80d9229fabe8754f80809265f89780769430dd21
Jonas Wielicki <j.wielicki@sotecware.net>
2018-05-29 07:32:42 +0200

    Re-work database initialisation

--- alembic/versions/0c17fe0f84bd_initialize_database.py
+++ alembic/versions/0c17fe0f84bd_initialize_database.py
@@ -1,18 +1,16 @@
 """initialize database
 
-Revision ID: 7591d5f3c41c
+Revision ID: 0c17fe0f84bd
 Revises:
-Create Date: 2018-05-28 17:14:50.907775
+Create Date: 2018-05-29 07:32:05.104046
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-from muchopper.common.model import JID
-
 
 # revision identifiers, used by Alembic.
-revision = '7591d5f3c41c'
+revision = '0c17fe0f84bd'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -26,14 +24,14 @@ def upgrade():
                   nullable=False,
                   autoincrement=True),
         sa.Column("domain",
-                  sa.LargeBinary(1023),
+                  sa.Unicode(1023),
                   unique=True,
                   nullable=False),
     )
 
     op.create_table(
         "muc",
-        sa.Column("address", sa.LargeBinary(3071),
+        sa.Column("address", sa.Unicode(3071),
                   primary_key=True,
                   nullable=False),
         sa.Column("domain_id", sa.Integer,
@@ -47,8 +45,6 @@ def upgrade():
                   nullable=True),
         sa.Column("moving_average_last_update", sa.DateTime,
                   nullable=True),
-        sa.Column("last_message_ts", sa.DateTime,
-                  nullable=True),
         sa.Column("is_open", sa.Boolean,
                   nullable=False),
         sa.Column("was_kicked", sa.Boolean,
@@ -57,7 +53,7 @@ def upgrade():
 
     op.create_table(
         "public_muc",
-        sa.Column("address", sa.LargeBinary(3071),
+        sa.Column("address", sa.Unicode(3071),
                   sa.ForeignKey("muc.address",
                                 ondelete="CASCADE",
                                 onupdate="CASCADE"),
@@ -69,6 +65,30 @@ def upgrade():
                   nullable=True),
         sa.Column("description", sa.Unicode,
                   nullable=True),
+    )
+
+    op.create_index(
+        "muc_ix_domain_id",
+        "muc",
+        [
+            "domain_id",
+        ]
+    )
+
+    op.create_index(
+        "muc_ix_is_open",
+        "muc",
+        [
+            "is_open",
+        ]
+    )
+
+    op.create_index(
+        "muc_ix_nusers_moving_average",
+        "muc",
+        [
+            "nusers_moving_average",
+        ],
     )
 
 
