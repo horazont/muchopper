horazont/muchopper

commit 6f0d8d45783288b1de4ef88c410f816f305605df

Massively improve response times of db-heavy views
Apparently, constructing SQLalchemy mapped objects can be veeery
slow. I’ve seen differences of a factor of 5 between querying
db.session.query(model.MUC, model.PubliclyListedMUC) and the
new {view,api}_base_query functions.

The result is a bit more unwieldly to work with, but the changes
especially in the template were surprisingly tame.

So let’s go for this. I expect the load on the service to drop
considerably.

muchopper/common/queries.py
@@ -40,10 +40,41 @@ def base_query(session, *,
     return base_filter(q, include_closed=include_closed)
 
 
+def api_base_query(session):
+    return session.query(
+        model.MUC.address,
+        model.MUC.nusers_moving_average,
+        model.MUC.is_open,
+        model.MUC.anonymity_mode,
+        model.PubliclyListedMUC.name,
+        model.PubliclyListedMUC.description,
+        model.PubliclyListedMUC.language,
+    ).select_from(model.MUC).join(model.PubliclyListedMUC)
+
+
+def view_base_query(session):
+    return session.query(
+        model.MUC.address,
+        model.MUC.nusers_moving_average,
+        model.MUC.is_open,
+        model.MUC.anonymity_mode,
+        model.PubliclyListedMUC.name,
+        model.PubliclyListedMUC.description,
+        model.PubliclyListedMUC.language,
+        model.PubliclyListedMUC.web_chat_url,
+        model.PubliclyListedMUC.http_logs_url,
+        model.Avatar.address != None,  # NOQA
+    ).select_from(model.MUC).join(model.PubliclyListedMUC).outerjoin(
+        model.Avatar
+    )
+
+
 def common_query(session, *,
                  min_users=1,
+                 q=None,
                  **kwargs):
-    q = base_query(session, **kwargs)
+    if q is None:
+        q = base_query(session, **kwargs)
 
     if min_users > 0:
         q = q.filter(

muchopper/web/__init__.py
@@ -439,7 +439,8 @@ def room_page(page, per_page, **kwargs):
 def room_list(pageno=1):
     per_page = 25
     try:
-        page = room_page(pageno, per_page, with_avatar_flag=True)
+        page = room_page(pageno, per_page, with_avatar_flag=True,
+                         q=queries.view_base_query(db.session))
     except ValueError:
         return abort(400, "invalid page")
 
@@ -566,9 +567,9 @@ def search():
             no_keywords = True
         else:
             q = queries.common_query(
-                db.session,
+                None,
                 min_users=0,
-                with_avatar_flag=True,
+                q=queries.view_base_query(db.session),
             )
 
             q = queries.apply_search_conditions(
@@ -801,17 +802,18 @@ def contact():
 # API
 
 
-def room_to_json(muc, public_info):
+def room_to_json(info):
+    address, nusers, is_open, anonymity_mode, name, description, language = info
     result = {
-        "address": str(muc.address),
-        "nusers": round(muc.nusers_moving_average),
-        "is_open": muc.is_open,
-        "name": public_info.name,
-        "description": public_info.description,
-        "language": public_info.language,
+        "address": str(address),
+        "nusers": round(nusers),
+        "is_open": is_open,
+        "name": name,
+        "description": description,
+        "language": language,
     }
-    if muc.anonymity_mode is not None:
-        result["anonymity_mode"] = muc.anonymity_mode.value
+    if anonymity_mode is not None:
+        result["anonymity_mode"] = anonymity_mode.value
     return result
 
 
@@ -833,7 +835,8 @@ def api_rooms_unsafe():
         return abort(400)
 
     try:
-        page = room_page(pageno, per_page=200, include_closed=include_closed)
+        page = room_page(pageno, per_page=200, include_closed=include_closed,
+                         q=queries.api_base_query(db.session))
     except ValueError:
         return abort(400)
 
@@ -841,10 +844,7 @@ def api_rooms_unsafe():
         "total": page.total,
         "npages": page.pages,
         "page": page.page,
-        "items": [
-            room_to_json(muc, public_info)
-            for muc, public_info in page.items
-        ],
+        "items": list(map(room_to_json, page.items)),
     })
 
 
@@ -875,7 +875,11 @@ def api_rooms_safe():
     except ValueError:
         return abort(400)
 
-    q = queries.base_query(db.session, include_closed=include_closed)
+    q = queries.api_base_query(db.session)
+    q = queries.base_filter(
+        q,
+        include_closed=include_closed,
+    )
     if after is not None:
         q = q.filter(
             model.MUC.address > after
@@ -889,13 +893,9 @@ def api_rooms_safe():
     )
 
     q = q.limit(PAGE_SIZE)
-    results = list(q)
 
     return jsonify({
-        "items": [
-            room_to_json(muc, public_info)
-            for muc, public_info in results
-        ],
+        "items": list(map(room_to_json, q))
     })
 
 
@@ -976,8 +976,9 @@ def api_search():
         )
 
     q = queries.common_query(
-        db.session,
+        None,
         min_users=0,
+        q=queries.api_base_query(db.session),
     )
     if after is not None:
         q = q.filter(
@@ -986,7 +987,7 @@ def api_search():
 
     q = queries.apply_search_conditions(
         q,
-        keywords,
+        prepped_keywords,
         search_address,
         search_description,
         search_name,
@@ -995,24 +996,15 @@ def api_search():
     q = q.limit(101)
     results = list(q)
 
-    more = len(results) > 100
-
     items = []
     last_key = None
-    for muc, public_info in results[:100]:
-        anonymity_mode = None
-        if muc.anonymity_mode is not None:
-            anonymity_mode = muc.anonymity_mode.value
-        items.append({
-            "address": str(muc.address),
-            "is_open": muc.is_open,
-            "nusers": round(muc.nusers_moving_average),
-            "description": public_info.description,
-            "name": public_info.name,
-            "language": public_info.language,
-            "anonymity_mode": anonymity_mode,
-        })
-        last_key = muc.nusers_moving_average
+    for info in q:
+        items.append(room_to_json(info))
+        last_key = info[1]
+
+    more = len(items) > 100
+    if more:
+        items.pop()
 
     result = {
         "query": {

muchopper/web/templates/library.tpl
@@ -23,23 +23,11 @@
 {% endif %}
 {% endmacro %}
 
-{% macro room_name(muc, public_info, caller=None) -%}
-{%- if public_info.name and public_info.name != muc.address.localpart %}{{ public_info.name }}{% else %}{{ (muc.address.localpart | jid_unescape) or muc.address }}{% endif -%}
-{%- endmacro %}
-
 {% macro room_address(address, keywords=[], caller=None) -%}
 {%- if address.localpart -%}
 <span class="address"><span class="localpart">{{ address.localpart | jid_unescape | highlight(keywords) }}</span><span class="at">@</span><span class="domain">{{ address.domain | highlight(keywords) }}</span></span>
 {%- else -%}
 {{ address | highlight(keywords) }}
-{%- endif -%}
-{%- endmacro %}
-
-{% macro room_label(muc, public_info, keywords=[]) -%}
-{%- if public_info.name and public_info.description and public_info.name != public_info.description and public_info.name != muc.address.localpart -%}
-<span class="name">{{ public_info.name | highlight(keywords) }}</span><span class="address-suffix"> ({{ muc.address | highlight(keywords) }})</span>
-{%- else -%}
-{%- call room_address(muc.address, keywords=keywords) -%}{%- endcall -%}
 {%- endif -%}
 {%- endmacro %}
 
@@ -62,47 +50,44 @@
 {% endmacro %}
 
 {% macro room_table(items, keywords=[], caller=None) %}
-<ol class="roomlist">{% for muc, public_info, has_avatar in items %}
-	{% set nusers = (muc.nusers_moving_average or muc.nusers) | round %}
-	{% if public_info.name != muc.address.localpart and public_info.name %}
-	{% set name = public_info.name %}
+<ol class="roomlist">{% for address, nusers, is_open, anonymity_mode, db_name, descr, db_language, web_chat_url, http_logs_url, has_avatar in items %}
+	{% set nusers = nusers | round %}
+	{% if db_name != address.localpart and db_name %}
+	{% set name = db_name %}
 	{% else %}
-	{% set name = (muc.address.localpart | jid_unescape) or (muc.address | string) %}
+	{% set name = (address.localpart | jid_unescape) or (address | string) %}
 	{% endif %}
-	{% set descr = public_info.description or public_info.subject %}
-	{% set show_descr = descr and descr != muc.address.localpart %}
-	{% set show_lang = public_info.language %}
-	{% set is_nonanon = not muc.anonymity_mode or muc.anonymity_mode.value == "none" %}
-	{% set is_closed = not muc.is_open %}
-	{% set web_chat_url = public_info.web_chat_url %}
-	{% set http_logs_url = public_info.http_logs_url %}
-	{% set set_lang_attr = public_info.language %}
+	{% set show_descr = descr and descr != address.localpart %}
+	{% set show_lang = db_language %}
+	{% set is_nonanon = not anonymity_mode or anonymity_mode.value == "none" %}
+	{% set is_closed = not is_open %}
+	{% set set_lang_attr = db_language %}
 	<li class="roomcard">
 		{# this is aria-hidden; we put the number of online users inline with the main content of the room card as a11y text #}
 		<div class="avatar-column" aria-hidden="true">
-			<div class="avatar">{% call avatar(has_avatar, muc.address) %}{{ name }}{% endcall %}</div>
+			<div class="avatar">{% call avatar(has_avatar, address) %}{{ name }}{% endcall %}</div>
 			<div class="expand"></div>
 			<div class="nusers" title="Number of users online">{% call icon("users") %}{% endcall %}<span class="expand"></span><span data-content="{{ (nusers | pretty_number_info)['short'] }}"></span></div>
 			<div class="expand"></div>
 		</div>
 		<div class="main">
-			<h3 class="title"{% if set_lang_attr %} lang="{{ public_info.language }}"{% endif %}><a href="xmpp:{{ muc.address }}?join" rel="nofollow">{{ name | highlight(keywords) }}</a></h3>
-			<div class="addr"><a href="xmpp:{{ muc.address }}?join" rel="nofollow">{% call room_address(muc.address, keywords=keywords) %}{% endcall %}</a>{%- call clipboard_button() %}{{ muc.address }}{% endcall -%}</div>
+			<h3 class="title"{% if set_lang_attr %} lang="{{ db_language }}"{% endif %}><a href="xmpp:{{ address }}?join" rel="nofollow">{{ name | highlight(keywords) }}</a></h3>
+			<div class="addr"><a href="xmpp:{{ address }}?join" rel="nofollow">{% call room_address(address, keywords=keywords) %}{% endcall %}</a>{%- call clipboard_button() %}{{ address }}{% endcall -%}</div>
 			<p class="a11y-text">{{ (nusers | pretty_number_info)['accessible'] }} users online</p>
 			{% if show_descr -%}
-			<p class="descr"{% if set_lang_attr %} lang="{{ public_info.language }}"{% endif %}>{{ descr | highlight(keywords) }}</p>
+			<p class="descr"{% if set_lang_attr %} lang="{{ db_language }}"{% endif %}>{{ descr | highlight(keywords) }}</p>
 			{%- endif %}
 			{%- if show_lang or is_nonanon or is_closed -%}
 			<div><ul class="inline slim">
-			{%- if show_lang -%}<li title="Primary room language" class="with-tooltip">{% call icon("lang1", alt="Primary language:") %}{% endcall %} {{ public_info.language | prettify_lang }}</li>{%- endif -%}
+			{%- if show_lang -%}<li title="Primary room language" class="with-tooltip">{% call icon("lang1", alt="Primary language:") %}{% endcall %} {{ db_language | prettify_lang }}</li>{%- endif -%}
 			{%- if is_nonanon -%}{% call nonanon_marker() %}{% endcall %}{%- endif -%}
 			{%- if is_closed -%}{% call closed_marker() %}{% endcall %}{%- endif -%}
 			</ul></div>
 			{%- endif -%}
 			{%- if web_chat_url or http_logs_url -%}
 			<div><ul class="actions">
-			{%- if web_chat_url %}{% call join_url(web_chat_url) %}{% call room_name(muc, public_info) %}{% endcall %}{% endcall %}{% endif -%}
-			{%- if http_logs_url %}{% call logs_url(http_logs_url) %}{% call room_name(muc, public_info) %}{% endcall %}{% endcall %}{% endif -%}</ul></div>
+			{%- if web_chat_url %}{% call join_url(web_chat_url) %}{{ name }}{% endcall %}{% endif -%}
+			{%- if http_logs_url %}{% call logs_url(http_logs_url) %}{{ name }}{% endcall %}{% endif -%}</ul></div>
 			{%- endif -%}
 		</div>
 	</li>

commit 2161868d58c0dc893c34590a878ec9a502510756

Do not include closed rooms in results by default
This fixes a regression introduced with the refactor in 6f0d8d4.

muchopper/common/queries.py
@@ -40,8 +40,8 @@ def base_query(session, *,
     return base_filter(q, include_closed=include_closed)
 
 
-def api_base_query(session):
-    return session.query(
+def api_base_query(session, **kwargs):
+    q = session.query(
         model.MUC.address,
         model.MUC.nusers_moving_average,
         model.MUC.is_open,
@@ -50,10 +50,11 @@ def api_base_query(session):
         model.PubliclyListedMUC.description,
         model.PubliclyListedMUC.language,
     ).select_from(model.MUC).join(model.PubliclyListedMUC)
+    return base_filter(q, **kwargs)
 
 
-def view_base_query(session):
-    return session.query(
+def view_base_query(session, **kwargs):
+    q = session.query(
         model.MUC.address,
         model.MUC.nusers_moving_average,
         model.MUC.is_open,
@@ -67,6 +68,7 @@ def view_base_query(session):
     ).select_from(model.MUC).join(model.PubliclyListedMUC).outerjoin(
         model.Avatar
     )
+    return base_filter(q, **kwargs)
 
 
 def common_query(session, *,

muchopper/web/__init__.py
@@ -917,6 +917,7 @@ def api_search():
         search_address = payload.get("sinaddr", True) is True
         search_description = payload.get("sindescr", True) is True
         search_name = payload.get("sinname", True) is True
+        include_closed = payload.get("include_closed", True) is True
         min_users = payload.get("min_users", 0)
         after = payload.get("after", None)
     except KeyError as e: