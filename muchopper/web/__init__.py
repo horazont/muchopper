horazont/muchopper

commit 8a978d096eda3d336b3cf7772fa5f256634adc38

Properly handle database entries with missing nusers information
Those exist when a crawl cannot complete. Caused two
500 Internal Server Errors today. This makes it show a
questionmark/"unknown" in the web interface and return 0 in the
API.

muchopper/web/__init__.py
@@ -312,6 +312,12 @@ def jid_unescape_filter(s):
 
 @app.template_filter("pretty_number_info")
 def pretty_number_info(n):
+    if n is None:
+        return {
+            "short": "?",
+            "accessible": "unknown",
+        }
+
     if n < 0.5:
         return {
             "short": "0",
@@ -806,7 +812,7 @@ def room_to_json(info):
     address, nusers, is_open, anonymity_mode, name, description, language = info
     result = {
         "address": str(address),
-        "nusers": round(nusers),
+        "nusers": round(nusers) if nusers is not None else 0,
         "is_open": is_open,
         "name": name,
         "description": description,

muchopper/web/templates/library.tpl
@@ -51,7 +51,7 @@
 
 {% macro room_table(items, keywords=[], caller=None) %}
 <ol class="roomlist">{% for address, nusers, is_open, anonymity_mode, db_name, descr, db_language, web_chat_url, http_logs_url, has_avatar in items %}
-	{% set nusers = nusers | round %}
+	{% set nusers = nusers | round if nusers is not none else None %}
 	{% if db_name != address.localpart and db_name %}
 	{% set name = db_name %}
 	{% else %}

commit dfc416beeaccc22f95c3693bec3f9cd4c223c500

Ensure that include_closed works correctly for unsafe room API
This fixes a regression introduced by 8a978d0 where the
include_closed argument would be ignored.

muchopper/web/__init__.py
@@ -881,10 +881,9 @@ def api_rooms_safe():
     except ValueError:
         return abort(400)
 
-    q = queries.api_base_query(db.session)
-    q = queries.base_filter(
-        q,
-        include_closed=include_closed,
+    q = queries.api_base_query(
+        db.session,
+        include_closed=include_closed
     )
     if after is not None:
         q = q.filter(