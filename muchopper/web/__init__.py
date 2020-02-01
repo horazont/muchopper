2068867ae74551eed790aff3e44478a6e64f4924
Jonas Schäfer <j.wielicki@sotecware.net>
2020-02-01 12:28:24 +0100

    Calculate and render language statistics
    Fixes #45.

--- muchopper/web/__init__.py
+++ muchopper/web/__init__.py
@@ -745,6 +745,66 @@ def statistics():
         }]
     }
 
+    languages_raw = db.session.query(
+        sqlalchemy.func.count().label("total"),
+        model.PubliclyListedMUC.language,
+    ).select_from(
+        model.PubliclyListedMUC
+    ).group_by(
+        model.PubliclyListedMUC.language,
+    ).order_by(
+        sqlalchemy.text("total DESC"),
+    )
+
+    languages = []
+    unspec_total = 0
+    spec_total = 0
+    for total, lang_raw in languages_raw:
+        if lang_raw is None:
+            unspec_total += total
+            continue
+
+        pretty_name = prettify_lang(lang_raw, fallback=False)
+        if pretty_name is None:
+            unspec_total += total
+            continue
+
+        languages.append(
+            (total, lang_raw, pretty_name)
+        )
+        spec_total += total
+
+    if unspec_total is not None:
+        languages.append((unspec_total, None, None))
+
+    pruned_languages = [
+        (total, name, colour.text_to_colour(tag))
+        for total, tag, name in languages
+        if tag is not None
+    ]
+
+    language_chart_cfg = {
+        "labels": [name for _, name, _ in pruned_languages],
+        "datasets": [{
+            "label": "Occurences",
+            "data": [total for total, _, _ in pruned_languages],
+            "backgroundColor": [
+                "rgba({:.0f}, {:.0f}, {:.0f}, 0.8)".format(
+                    r*255, g*255, b*255
+                )
+                for _, _, (r, g, b) in pruned_languages
+            ],
+            "borderColor": [
+                "rgba({:.0f}, {:.0f}, {:.0f}, 1.0)".format(
+                    r*255, g*255, b*255
+                )
+                for _, _, (r, g, b) in pruned_languages
+            ],
+            "borderWidth": 1
+        }]
+    }
+    language_chart_sum = spec_total
+
     return render_template(
         "stats.html",
         softwares=softwares,
@@ -754,6 +814,9 @@ def statistics():
         unknown_service_types=unknown_service_types,
         software_version_chart_cfg=software_version_chart_cfg,
         software_version_chart_sum=software_version_chart_sum,
+        languages=languages,
+        language_chart_cfg=language_chart_cfg,
+        language_chart_sum=language_chart_sum,
         **common_metrics,
     )
 
